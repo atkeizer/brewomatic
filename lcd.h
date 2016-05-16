1f0198d7211bc7c251b56cbf774ac4ad4dcc92d3
Albert Keizer <atkeizer@gmail.com>
2016-05-16 08:50:14 +0000

    lcd_puts_p added

--- lcd.h
+++ lcd.h
@@ -1,13 +1,17 @@
 /*
 
-$Header$
+$Header: /home/atkeizer/avr/brouwtomaat/lcd.h,v 1.1 2016/04/30 08:55:00 atkeizer Exp atkeizer $
 
-$Log$
+$Log: lcd.h,v $
+Revision 1.1  2016/04/30 08:55:00  atkeizer
+Initial revision
+
 
 */
 
 void lcd_init(void);
 void lcd_puts(char *data);
+void lcd_puts_p(const char *prog_data);
 void lcd_gotoxy(char x, char y);
 void lcd_write_byte(char byte);
 void lcd_write_nibble(char byte);
