216527cff0d6b9d8d8482e2c176602e75ac4587b
Albert Keizer <atkeizer@gmail.com>
2017-10-09 08:20:34 +0000

    Initial revision

---
+++ menu.h
@@ -0,0 +1,19 @@
+/*
+
+$Header$
+
+$Log$
+
+*/
+
+void process_menu(uint8_t button);
+void menu();
+void do_cont();
+void pump();
+void heater();
+void settings();
+void mash_steps();
+void mash_sched();
+void do_set_pid();
+
+
